5e3bd0c601d6f985a500ddb3d4436c08149371aa
Adrian Marimon <marimon.dev@Adrians-MacBook-Pro.local>
2021-09-07 18:38:10 -0400

    signin ready

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { Auth, API, graphqlOperation } from 'aws-amplify'
+import awsconfig from './aws-exports'
+import { AmplifySignOut,  withAuthenticator } from '@aws-amplify/ui-react'
+import * as queries from './graphql/queries'
+import * as mutations from './graphql/mutations'
+
+Auth.configure(awsconfig)
+API.configure(awsconfig)
 
 function App() {
+
+  const allTodos = API.graphql(graphqlOperation(queries.listTodos))
+  const oneTodo = API.graphql(graphqlOperation(queries.getTodo, {id: 'abf16ffc-e9ff-463b-8f1d-177befb846bc' }))
+
+  console.log(oneTodo);
+
+  Auth.currentAuthenticatedUser({ bypassCache: false })
+    .then((user) => {
+      console.log(`User: ${JSON.stringify(user)}`);
+      const todo = { name: user['username'], description: "bear5's description" }
+      const newTodo = API.graphql(graphqlOperation(mutations.createTodo, {input: todo }))
+    })
+    .catch(err => console.log(err))
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,8 +40,10 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <AmplifySignOut/>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
